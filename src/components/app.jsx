185eac09c082dd0a209af381b27c808650f9555d
Jean Colombel <jeancolombel93@gmail.com>
2021-06-30 15:33:51 -0400

    DOne

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 import flats from '../../data/flats.js'
+import FlatList from './flat_list.jsx'
+import Marker from './marker.jsx'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      flats: flats
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
     };
   }
 
   render() {
     return(
-      <FlatList flats={this.flats}/>
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}/>
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
     )
   }
+
+
 }
 
 export default App;
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 
-class Card extends Component {
+class Flat extends Component {
   render() {
+    const flatStyle = {
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)),
+                        url(${this.props.flat.imageUrl})`
+    }
+
+    const flatClass = this.props.selected ? "card active" : "card"
+
     return(
-      <div className="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url({this.props.imageUrl});">
-        <div class="card-category">150 EUR</div>
-        <div class="card-description">
-          <h2>Super 60m2 in trendy neighborhood!</h2>
+      <div className={flatClass} style={flatStyle}>
+        <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
+        <div className="card-description">
+          <h2>{this.props.flat.name}</h2>
         </div>
-        <a class="card-link" href="#"></a>
+        <a className="card-link" href="#" onClick={this.handleClick}></a>
       </div>
     )
   }
 
+  handleClick = (e) => {
+    this.props.selectFlat(this.props.index)
+  }
 }
 
-export default Card;
+export default Flat;
